0628bc79bf714668ce7085f059bfc8c01cabe11f
Guilherme Reis Araújo <guih2127@gmail.com>
2022-01-16 15:50:33 -0300

    Creation of Context for Database and the seed method for the Products.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(connectionString);
+
+            Products = database.GetCollection<Product>(connectionString);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
