74ec4b21e0e219798be80aea5290f06eeb16fdaf
Guilherme Reis Araújo <guih2127@gmail.com>
2022-01-24 22:30:07 -0300

    Discount.proto for the Discount.Grpc

--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -33,8 +33,6 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
-
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
